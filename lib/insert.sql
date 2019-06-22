72a9c6c27ff1b6fda14939baaa6b584d9c80d8e7
earthctzn <earthctzn1@gmail.com>
2019-06-22 01:44:52 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 3, "male", "polkadot", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Rowdy", 1, "male", "magenta", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Tabitha", 6, "female", "lavender", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Sergeant Brown", 3, "male", "turqoise", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Melissa", 13, "female", "brown", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Grinch", 10, "male", "blue", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES ("Wendy", 6, "female", "pink", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES (NULL, 3, "male", "Maroon", "a bit agro", 1);
