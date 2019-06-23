21312e0ecfc8756b92a1d4feaa020f85dbfef26c
earthctzn <earthctzn1@gmail.com>
2019-06-23 12:17:35 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Mr. Chocolate", 3, "m", "polkadot", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Rowdy", 1, "m", "magenta", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Tabitha", 6, "f", "lavender", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Sergeant Brown", 3, "m", "turqoise", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Melissa", 13, "f", "brown", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Grinch", 10, "m", "blue", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES ("Wendy", 6, "f", "pink", "chill", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) 
-VALUES (NULL, 3, "m", "Maroon", "a bit agro", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 3, "m", "polkadot", "chill", 1), 
+("Rowdy", 1, "m", "magenta", "chill", 1), 
+("Tabitha", 6, "f", "lavender", "chill", 1), 
+("Sergeant Brown", 3, "m", "turqoise", "chill", 1), 
+("Melissa", 13, "f", "brown", "chill", 1), 
+("Grinch", 10, "m", "blue", "chill", 1), 
+("Wendy", 6, "f", "pink", "chill", 1), 
+(NULL, 3, "m", "Maroon", "a bit agro", 1);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,9 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) FROM bears GROUP BY color HAVING COUNT(color)=(
-    SELECT MAX(color_count) FROM(
-      SELECT color, COUNT(color) color_count FROM bears GROUP BY color));"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(color) DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
